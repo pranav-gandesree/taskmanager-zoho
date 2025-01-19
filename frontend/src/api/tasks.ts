77fc65db2a022829455cd9d9a4041d17835798b9
pranav <pranav8267@gmail.com>
2025-01-20 01:13:06 +0530

    add env variables

--- frontend/src/api/tasks.ts
+++ frontend/src/api/tasks.ts
@@ -1,6 +1,7 @@
+
 import axios from 'axios';
 
-const API_URL = 'https://taskmanager-60036587353.development.catalystserverless.in/server/todo-app/tasks';
+const API_URL = import.meta.env.VITE_API_URL
 
 // const API_URL = 'http://localhost:3000/server/todo-app/tasks';
 
