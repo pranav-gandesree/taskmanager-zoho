782b6d200d3e55a43b9ee8f92101a6f27a72a9e5
pranav <pranav8267@gmail.com>
2025-01-20 01:17:55 +0530

    change route

--- frontend/src/api/tasks.ts
+++ frontend/src/api/tasks.ts
@@ -7,22 +7,22 @@ const API_URL = import.meta.env.VITE_API_URL
 
 
 export const getTasks = async () =>{
-    const response = await axios.get(`${API_URL}/getTasks`);
+    const response = await axios.get(`${API_URL}/tasks/getTasks`);
     return response.data;   
 }
 
 export const createTask = async (task: { title: string; description: string; pending: boolean }) => {
-    const response = await axios.post(`${API_URL}/addTask`, task);
+    const response = await axios.post(`${API_URL}/tasks/addTask`, task);
     return response.data;
 };
 
 export const updateTask = async (task: { id: bigint ; title: string; description: string; pending: boolean }) => {
-    const response = await axios.put(`${API_URL}/updateTask/${task.id}`, task);
+    const response = await axios.put(`${API_URL}/tasks/updateTask/${task.id}`, task);
     return response.data;
 };
 
 export const deleteTask = async (ROWID: bigint) => {
-    const response = await axios.delete(`${API_URL}/deleteTask/${ROWID}`);
+    const response = await axios.delete(`${API_URL}/tasks/deleteTask/${ROWID}`);
     return response.data;
 };
 
